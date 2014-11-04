9a6ddc7a3a47fd22a5c273dcc5226f086c8a8245
Nicholas Brooks <nicholas@Nicholass-MacBook-Pro.local>
2014-11-03 22:57:45 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -9,6 +9,10 @@
 #import "CCSprite.h"
 
 @interface Grid : CCSprite
+- (void)evolveStep;
+@property (nonatomic, assign) int countNeighbors;
+@property (nonatomic, assign) int updateCreatures;
+@property (nonatomic, assign) BOOL isIndexValid;
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 @end
